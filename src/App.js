9d03abf471d0f877705f613f59d8e23dd4067afb
Ionatan Sala <sala.jonatan1995@gmail.com>
2017-04-21 07:33:55 +0100

    add antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 
 class App extends Component {
   render() {
     return (
       <section className="application-container">
-        <h1>Hello</h1>
+        <Button type="primary">Click Me</Button>
       </section>
     );
   }
