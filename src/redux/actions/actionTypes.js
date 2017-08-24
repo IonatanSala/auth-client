b11df176dc5fece19a464138f9e4b2156535fc7a
Ionatan Sala <sala.jonatan1995@gmail.com>
2017-08-24 21:29:34 +0100

    add endpoint based on env

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,8 +1,13 @@
-export const API_ENDPOINT = 'http://localhost:8000';
+export const API_ENDPOINT =
+  process.env.NODE_ENV === 'production'
+    ? 'http://localhost:123'
+    : 'http://localhost:8000';
+
 export const CREATE_ACCOUNT = 'CREATE_ACCOUNT';
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
-export const RESEND_CONFIRMATION_INSTRUCTIONS = 'RESEND_CONFIRMATION_INSTRUCTIONS';
+export const RESEND_CONFIRMATION_INSTRUCTIONS =
+  'RESEND_CONFIRMATION_INSTRUCTIONS';
 export const LOADING = 'LOADING';
 
 export const RESPONSE_STATUS = 'RESPONSE_STATUS';
